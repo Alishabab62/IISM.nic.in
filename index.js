8d455f7a373721cbece73b49f2c983a1c05e3052
alishabab <alishabab62@gmail.com>
2022-12-14 17:46:09 +0530

    New Site

--- index.js
+++ index.js
@@ -1,39 +1,16 @@
-//Learning Tab
-const learningBar=document.getElementById('learning-bar');
-const learningTab=document.getElementById('learning-subtab');
-learningTab.addEventListener('mouseover',()=>{
-    learningTab.style.display='block';
-})
-learningTab.addEventListener('mouseout',()=>{
-    learningTab.style.display='none';
-})
-learningBar.addEventListener('mouseover',()=>{
-    learningTab.style.display="block"
-});
-learningBar.addEventListener('mouseout',()=>{
-    learningTab.style.display="none"
-});
+const navBar=document.getElementById('nav-bar');
+const navTab=document.getElementById('nav');
+let flag=false;
+navBar.addEventListener('click',()=>{
+    if(flag==false){
+        flag=true
+        navTab.style.display='block';
+    console.log("hello")
+    }
+    else if(flag==true){
+        flag=false
+        navTab.style.display='none';
 
-//Faculty Tab
-const facultyTab=document.getElementById('faculty-subtab');
-const facultyBar=document.getElementById('faculty-bar');
-facultyTab.addEventListener('mouseover',()=>{
-    facultyTab.style.display='block';
+    }
+    
 })
-facultyTab.addEventListener('mouseout',()=>{
-    facultyTab.style.display='none';
-})
-facultyBar.addEventListener('mouseover',()=>{
-    facultyTab.style.display="block"
-});
-facultyBar.addEventListener('mouseout',()=>{
-    facultyTab.style.display="none"
-});
-
-
-//Home Page Button Change
-const home=document.getElementById('home');
-home.style.backgroundColor="#336699"
-home.style.color="white"
-
-//Logo Change
