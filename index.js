01028ff059fd76a16649fa596cc044c7cb0f3e0c
alishabab <alishabab62@gmail.com>
2022-12-20 19:19:07 +0530

    Menu and footer added

--- index.js
+++ index.js
@@ -2,15 +2,33 @@ const navBar=document.getElementById('nav-bar');
 const navTab=document.getElementById('nav');
 let flag=false;
 navBar.addEventListener('click',()=>{
-    if(flag==false){
+    if(flag===false){
         flag=true
         navTab.style.display='block';
-    console.log("hello")
     }
-    else if(flag==true){
+    else if(flag===true){
         flag=false
         navTab.style.display='none';
-
     }
     
-})
+});
+
+const sideBar=document.querySelector('.menu-bar');
+sideBar.addEventListener('click',myMove);
+
+function myMove() {
+    let id = null;
+    const elem = document.getElementById("men");  
+    console.log(elem);
+    let pos = 0;
+    clearInterval(id);
+    id = setInterval(frame, 2);
+    function frame() {
+      if (pos == 400) {
+        clearInterval(id);
+      } else {
+        pos=pos+5; 
+        elem.style.marginLeft = pos + "px"; 
+      }
+    }
+  }
